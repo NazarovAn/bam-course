7012d69cc3d4bef38f47d4c92ad365285b6f090c
NazarovAn <naatonchrome@gmail.com>
2020-10-26 16:44:29 +0400

    added fonts, header styled

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './blocks/page.css';
+import './blocks/header/header.css';
+import './blocks/header/header__menu.css';
+import './blocks/header/header-menu__logo.css';
+import './blocks/header/header-menu__sign.css';
+import './blocks/header/header__promo.css';
+import './blocks/header/header__find-partner-button.css';
